087b4e87206027c8b7afdcbc8628b722b56875ac
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-08-08 20:55:04 +0530

    final onboarding UI

--- app/src/main/java/com/itssagnikmukherjee/splashscreen/screens/OnboardingScreen.kt
+++ app/src/main/java/com/itssagnikmukherjee/splashscreen/screens/OnboardingScreen.kt
@@ -1,12 +1,22 @@
 package com.itssagnikmukherjee.splashscreen.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -14,8 +24,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.LottieComposition
@@ -29,33 +43,56 @@ import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 import com.itssagnikmukherjee.splashscreen.R
 import com.itssagnikmukherjee.splashscreen.ui.theme.mukta
-
+@Preview
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun OnboardingScreen() {
     val pagerState = rememberPagerState()
     val list = getList()
+    Row (
+        modifier = Modifier.fillMaxWidth().padding(20.dp).offset(x=-10.dp,y=40.dp),
+        horizontalArrangement = Arrangement.End,
+    ){
+        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = Color.Black
+        )){
+        Text(text = "SKIP", fontFamily = mukta, fontSize = 16.sp, fontWeight = FontWeight.Light, color = Color.Gray)
+        }
+    }
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ){
-        HorizontalPager(count = list.size, state = pagerState) {
+        HorizontalPager(count = list.size, state = pagerState, modifier = Modifier
+            .weight(1f)
+            .padding(10.dp)) {
             val composition by rememberLottieComposition(list[it].image)
             Column(
                 modifier = Modifier.fillMaxSize(.8f),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ){
-                Text(text = "${list[it].title}", fontFamily = mukta, fontWeight = FontWeight.ExtraBold, fontSize = 30.sp)
-                LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever)
-                Text(text = "${list[it].description}", fontFamily = mukta)
+                Text(text = "${list[it].title}", fontFamily = mukta, fontWeight = FontWeight.ExtraBold, fontSize = 38.sp, modifier = Modifier.fillMaxHeight(0.2f), lineHeight = 40.sp)
+                LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever, modifier = Modifier.fillMaxHeight(0.5f))
+                Text(text = "${list[it].description}", fontFamily = mukta, modifier = Modifier
+                    .fillMaxHeight(0.6f)
+                    .padding(top = 50.dp), style = TextStyle(
+                    textAlign = TextAlign.Justify,
+                    fontSize = 16.sp
+                ))
+
             }
+
         }
-        HorizontalPagerIndicator(pagerState=pagerState,pageCount = list.size, indicatorWidth = 20.dp, indicatorHeight = 15.dp)
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "")
+        Button(onClick = { /*TODO*/ }, modifier = Modifier.offset(y = -130.dp), colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black,
+            contentColor = Color.White
+        )) {
+            Icon(imageVector = Icons.Filled.ArrowForward, contentDescription = "")
         }
+        HorizontalPagerIndicator(pagerState=pagerState,pageCount = list.size, indicatorWidth = 20.dp, indicatorHeight = 15.dp, modifier = Modifier.offset(0.dp,-70.dp))
     }
 }
 
@@ -63,12 +100,22 @@ data class PagerContent(
     val image: LottieCompositionSpec,
     val title: String,
     val description: String,
+    val bgColor: Color
 )
 
 fun getList():List<PagerContent>{
     return listOf(
-        PagerContent(LottieCompositionSpec.RawRes(R.raw.onboarding2), "Explore Destinations", "Find and book amazing travel experiences with ease. From stunning beaches to bustling cities, your perfect getaway is just a tap away."),
-        PagerContent(LottieCompositionSpec.RawRes(R.raw.onboarding2), "Seamless Bookings", "Book flights, hotels, and activities effortlessly. Enjoy hassle-free travel planning experience with our comprehensive booking."),
-        PagerContent(LottieCompositionSpec.RawRes(R.raw.onboarding2), "Best Recommendations", "Receive personalized recommendations based on your preferences. Let us help you create unforgettable travel memories."),
+        PagerContent(
+            LottieCompositionSpec.RawRes(R.raw.onboarding1),
+            "Explore all Destinations",
+            "Find and book amazing travel experiences with ease. From stunning beaches to bustling cities, your perfect getaway is just a tap away.",
+            Color.White
+        ),
+        PagerContent(LottieCompositionSpec.RawRes(R.raw.onboarding2), "Seamless fast Bookings", "Book flights, hotels, and activities effortlessly. Enjoy hassle-free travel planning experience with our comprehensive booking.",
+            Color.White
+        ),
+        PagerContent(LottieCompositionSpec.RawRes(R.raw.onboarding3), "The Best Preferences", "Receive personalized recommendations based on your preferences. Let us help you create unforgettable travel memories.",
+            Color.White
+        ),
     )
 }
